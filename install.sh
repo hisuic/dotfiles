80bd2fc2279211598090fa504c903f4415315c26
h1neparrot <henryharlay@gmail.com>
2024-03-25 07:40:22 +0900

    Make backup directory and move original config files there

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
+# make backup directory
+mkdir ~/dotfiles/backup
+
 # bashrc
 if [ -f ~/.bashrc ]; then
-	command mv ~/.bashrc ~/dotfiles
+	command mv ~/.bashrc ~/dotfiles/backup
 	command ln -s ~/dotfiles/.bashrc ~/.bashrc
 fi
 
 # bash_aliases
 if [ -f ~/.bash_aliases ]; then
-        command mv ~/.bash_aliases ~/dotfiles
-	command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+    command mv ~/.bash_aliases ~/dotfiles/backup
+    command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 fi 
 
 # vimrc
 if [ -f ~/.vimrc ]; then
-        command mv ~/.vimrc ~/dotfiles
+    command mv ~/.vimrc ~/dotfiles/backup
 	command ln -s ~/dotfiles/.vimrc ~/.vimrc
 fi 
