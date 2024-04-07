6e35cb4258fe0071c0757214347ebc4dcfc4f20f
h1ne <henryharlay@gmail.com>
2024-04-07 10:47:32 +0900

    Fix bug

--- install.sh
+++ install.sh
@@ -9,16 +9,22 @@ fi
 if [ -f $HOME/.bashrc ]; then
 	command mv ~/.bashrc ~/dotfiles/backup
 	command ln -s ~/dotfiles/.bashrc ~/.bashrc
+else
+	command ln -s ~/dotfiles/.bashrc ~/.bashrc
 fi
 
 # bash_aliases
 if [ -f $HOME/.bash_aliases ]; then
 	command mv ~/.bash_aliases ~/dotfiles/backup
 	command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+else
+	command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 fi 
 
 # vimrc
 if [ -f $HOME/.vimrc ]; then
 	command mv ~/.vimrc ~/dotfiles/backup
+	command ln -s ~/dotfiles/.vimrc ~/.vimrc
+else
 	command ln -s ~/dotfiles/.vimrc ~/.vimrc
 fi 
