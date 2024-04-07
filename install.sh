f672290bee67319fa06a97adbaa7fea560e91673
h1ne <henryharlay@gmail.com>
2024-04-07 10:43:56 +0900

    Edit ~ to /home/hisui

--- install.sh
+++ install.sh
@@ -6,19 +6,19 @@ if [ ! -d "$HOME/dotfiles/backup" ]; then
 fi
 
 # bashrc
-if [ -f ~/.bashrc ]; then
+if [ -f $HOME/.bashrc ]; then
 	command mv ~/.bashrc ~/dotfiles/backup
 	command ln -s ~/dotfiles/.bashrc ~/.bashrc
 fi
 
 # bash_aliases
-if [ -f ~/.bash_aliases ]; then
+if [ -f $HOME/.bash_aliases ]; then
 	command mv ~/.bash_aliases ~/dotfiles/backup
 	command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 fi 
 
 # vimrc
-if [ -f ~/.vimrc ]; then
+if [ -f $HOME/.vimrc ]; then
 	command mv ~/.vimrc ~/dotfiles/backup
 	command ln -s ~/dotfiles/.vimrc ~/.vimrc
 fi 
