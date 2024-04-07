99ad4729adf9cdfcf5521dce7704180d9164e89c
h1ne <henryharlay@gmail.com>
2024-04-07 10:31:21 +0900

    Fix indent

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # make backup directory
 if [ ! -d "$HOME/dotfiles/backup" ]; then
-    mkdir ~/dotfiles/backup
+	mkdir ~/dotfiles/backup
 fi
 
 # bashrc
@@ -13,12 +13,12 @@ fi
 
 # bash_aliases
 if [ -f ~/.bash_aliases ]; then
-    command mv ~/.bash_aliases ~/dotfiles/backup
-    command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+	command mv ~/.bash_aliases ~/dotfiles/backup
+	command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 fi 
 
 # vimrc
 if [ -f ~/.vimrc ]; then
-    command mv ~/.vimrc ~/dotfiles/backup
-    command ln -s ~/dotfiles/.vimrc ~/.vimrc
+	command mv ~/.vimrc ~/dotfiles/backup
+	command ln -s ~/dotfiles/.vimrc ~/.vimrc
 fi 
