1ef0133e99ed17736363baa750786855ececf3b4
hisui <henryharlay@gmail.com>
2025-03-17 16:27:30 +0900

    remove neofetch, nvim setting

--- install.sh
+++ install.sh
@@ -1,46 +1,28 @@
 # make backup directory
 if [ ! -d "$HOME/dotfiles/backup" ]; then
-	mkdir ~/dotfiles/backup
+  mkdir ~/dotfiles/backup
 fi
 
 # bashrc
 if [ -f $HOME/.bashrc ]; then
-	command mv ~/.bashrc ~/dotfiles/backup
-	command ln -s ~/dotfiles/.bashrc ~/.bashrc
+  command mv ~/.bashrc ~/dotfiles/backup
+  command ln -s ~/dotfiles/.bashrc ~/.bashrc
 else
-	command ln -s ~/dotfiles/.bashrc ~/.bashrc
+  command ln -s ~/dotfiles/.bashrc ~/.bashrc
 fi
 
 # bash_aliases
 if [ -f $HOME/.bash_aliases ]; then
-	command mv ~/.bash_aliases ~/dotfiles/backup
-	command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+  command mv ~/.bash_aliases ~/dotfiles/backup
+  command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 else
-	command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
-fi 
+  command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+fi
 
 # vimrc
 if [ -f $HOME/.vimrc ]; then
-	command mv ~/.vimrc ~/dotfiles/backup
-	command ln -s ~/dotfiles/.vimrc ~/.vimrc
+  command mv ~/.vimrc ~/dotfiles/backup
+  command ln -s ~/dotfiles/.vimrc ~/.vimrc
 else
-	command ln -s ~/dotfiles/.vimrc ~/.vimrc
-fi 
-
-# neofetch
-if [ -f $HOME/.config/neofetch/config.conf ]; then
-	mkdir ~/dotfiles/backup/neofetch
-	command mv ~/.config/neofetch/config.conf ~/dotfiles/backup/neofetch
-	command ln -s ~/dotfiles/.config/neofetch/config.conf ~/.config/neofetch/config.conf
-else
-	command ln -s ~/dotfiles/.config/neofetch/config.conf ~/.config/neofetch/config.conf
-fi 
-
-
-# Neovim
-#if [ -f $HOME/.config/nvim ]; then
-#	command mv ~/.config/nvim ~/dotfiles/backup
-#	command cp -r ~/dotfiles/LazyVim ~/.config/nvim
-#else
-#	command cp -r ~/dotfiles/LazyVim ~/.config/nvim
-#fi
+  command ln -s ~/dotfiles/.vimrc ~/.vimrc
+fi
