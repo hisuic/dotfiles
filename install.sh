86103eb8defff690888546f60937baf286a9c965
hisui <henryharlay@gmail.com>
2025-03-17 16:42:27 +0900

    fix path

--- install.sh
+++ install.sh
@@ -6,23 +6,23 @@ fi
 # bashrc
 if [ -f $HOME/.bashrc ]; then
   command mv ~/.bashrc ~/dotfiles/backup
-  command ln -s ~/dotfiles/.bashrc ~/.bashrc
+  command ln -s ~/dotfiles/home/bash/.bashrc ~/.bashrc
 else
-  command ln -s ~/dotfiles/.bashrc ~/.bashrc
+  command ln -s ~/dotfiles/home/bash/.bashrc ~/.bashrc
 fi
 
 # bash_aliases
 if [ -f $HOME/.bash_aliases ]; then
   command mv ~/.bash_aliases ~/dotfiles/backup
-  command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+  command ln -s ~/dotfiles/home/bash/.bash_aliases ~/.bash_aliases
 else
-  command ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+  command ln -s ~/dotfiles/home/bash/.bash_aliases ~/.bash_aliases
 fi
 
 # vimrc
 if [ -f $HOME/.vimrc ]; then
   command mv ~/.vimrc ~/dotfiles/backup
-  command ln -s ~/dotfiles/.vimrc ~/.vimrc
+  command ln -s ~/dotfiles/home/vim/.vimrc ~/.vimrc
 else
-  command ln -s ~/dotfiles/.vimrc ~/.vimrc
+  command ln -s ~/dotfiles/home/vim/.vimrc ~/.vimrc
 fi
